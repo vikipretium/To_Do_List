ad45f9f29b4c6b4149cf2e21cbfc6888cb2d80bd
vikipretium <vighneshpasupathy@gmail.com>
2022-02-16 19:14:52 +0530

    add config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: path.resolve(__dirname, './src/levy.html'),
+      template: path.resolve(__dirname, './src/index.html'),
     }),
   ],
   output: {
