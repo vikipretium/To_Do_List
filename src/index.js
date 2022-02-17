57b8b2e3b94f9993226ac6925d07f7d9835b95c4
vikipretium <vighneshpasupathy@gmail.com>
2022-02-17 17:09:20 +0530

    add content structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  element.classList.add('container');
+// input section
+import Image from './asset/images/add-grey.svg';
+import Image1 from './asset/images/3dots-grey.svg';
 
-  element.document.createElement('ul');
+const render = `<a href="#"><img src="${Image}" alt="img"></a>`;
+const addButton = document.getElementById('add-button');
+addButton.innerHTML = render;
 
+// list section
+const ListContent = [{
+  id: 0,
+  description: 'task1',
+  status: false,
+},
+{
+  id: 1,
+  description: 'task2',
+  status: false,
+}, {
+  id: 2,
+  description: 'task3',
+  status: false,
+}, {
+  id: 4,
+  description: 'task4',
+  status: false,
+},
+];
 
-
-  return element;
+// iteration through array
+for (let i = 0; i < ListContent.length; i += 1) {
+  const ListElement = document.createElement('li');
+  const ListTemplate = `<div class="list-content">
+    <input type="checkbox">
+    <span>${ListContent[i].description}</span>
+    </div>
+    <img src="${Image1}" alt="dots">`;
+  ListElement.innerHTML = ListTemplate;
+  const ListSection = document.getElementById('ulList');
+  ListSection.appendChild(ListElement);
 }
-
-document.body.appendChild(component());
