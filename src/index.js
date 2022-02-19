be613f895e9993fcfbfb7bae5740d69be823187e
vikipretium <vighneshpasupathy@gmail.com>
2022-02-19 16:17:29 +0530

    modify js code

--- src/index.js
+++ src/index.js
@@ -1,43 +1,46 @@
-import './style.css';
+import "./style.css";
 
 // input section
-import Image from './asset/images/add-grey.svg';
-import Image1 from './asset/images/3dots-grey.svg';
+import Image from "./asset/images/add-grey.svg";
+import Image1 from "./asset/images/3dots-grey.svg";
 
 const render = `<a href="#"><img src="${Image}" alt="img"></a>`;
-const addButton = document.getElementById('add-button');
+const addButton = document.getElementById("add-button");
 addButton.innerHTML = render;
 
 // list section
-const ListContent = [{
-  id: 0,
-  description: 'task1',
-  status: false,
-},
-{
-  id: 1,
-  description: 'task2',
-  status: false,
-}, {
-  id: 2,
-  description: 'task3',
-  status: false,
-}, {
-  id: 4,
-  description: 'task4',
-  status: false,
-},
+const ListContent = [
+  {
+    id: 0,
+    description: "task1",
+    status: false,
+  },
+  {
+    id: 1,
+    description: "task2",
+    status: false,
+  },
+  {
+    id: 2,
+    description: "task3",
+    status: false,
+  },
+  {
+    id: 4,
+    description: "task4",
+    status: false,
+  },
 ];
 
 // iteration through array
-for (let i = 0; i < ListContent.length; i += 1) {
-  const ListElement = document.createElement('li');
-  const ListTemplate = `<div class="list-content">
-    <input type="checkbox">
-    <span>${ListContent[i].description}</span>
+const todoInput = document.querySelector(".todo-input");
+
+const ListElement = document.createElement("li");
+const ListTemplate = `<div class="list-content">
+    <input type="checkbox" class="${ListContent[i].id}">
+    <span>${todoInput.value}</span>
     </div>
     <img src="${Image1}" alt="dots">`;
-  ListElement.innerHTML = ListTemplate;
-  const ListSection = document.getElementById('ulList');
-  ListSection.appendChild(ListElement);
-}
+ListElement.innerHTML = ListTemplate;
+const ListSection = document.getElementById("ulList");
+ListSection.appendChild(ListElement);
