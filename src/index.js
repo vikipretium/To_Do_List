111d3c4425667f0d499eb01eef31cd79ab7a5360
vikipretium <vighneshpasupathy@gmail.com>
2022-02-21 18:41:35 +0530

    replace map with forEach function

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
-import "./style.css";
+import './style.css';
 
 // input section
-import Image from "./asset/images/add-grey.svg";
-import Image1 from "./asset/images/3dots-grey.svg";
+import Image from './asset/images/add-grey.svg';
+import Image1 from './asset/images/3dots-grey.svg';
 
 const render = `<a href="#"><img src="${Image}" alt="img"></a>`;
-const addButton = document.getElementById("add-button");
+const addButton = document.getElementById('add-button');
 addButton.innerHTML = render;
 
 // list section
 const ListContent = [
   {
     id: 0,
-    description: "task1",
+    description: 'task1',
     status: false,
   },
   {
     id: 1,
-    description: "task2",
+    description: 'task2',
     status: false,
   },
   {
     id: 2,
-    description: "task3",
+    description: 'task3',
     status: false,
   },
   {
     id: 3,
-    description: "task4",
+    description: 'task4',
     status: false,
   },
 ];
 
 // iteration through array
-ListContent.map((todo) => {
-  const ListElement = document.createElement("li");
+ListContent.forEach((todo) => {
+  const ListElement = document.createElement('li');
   ListElement.id = todo.id;
   const ListTemplate = `<div class="list-content">
     <input type="checkbox">
@@ -42,6 +42,6 @@ ListContent.map((todo) => {
     </div>
     <img src="${Image1}" alt="dots">`;
   ListElement.innerHTML = ListTemplate;
-  const ListSection = document.getElementById("ulList");
+  const ListSection = document.getElementById('ulList');
   ListSection.appendChild(ListElement);
 });
