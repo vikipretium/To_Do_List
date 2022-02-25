2c1e1a1486c2d954a32ec0fd90bede48d3371ed2
vikipretium <vighneshpasupathy@gmail.com>
2022-02-25 19:22:25 +0530

    add separate module for status update

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
-import './style.css';
+import "./style.css";
 // input section
-import Image from './asset/images/add-grey.svg';
-import Image1 from './asset/images/3dots-grey.svg';
-import Image2 from './asset/images/trash.svg';
+import Image from "./asset/images/add-grey.svg";
+import Image1 from "./asset/images/3dots-grey.svg";
+import Image2 from "./asset/images/trash.svg";
+import statusCheckBox from "./asset/modules/statusCheck.js";
 
 const render = `<a href="#"><img src="${Image}" alt="img"></a>`;
-const addButton = document.getElementById('add-button');
+const addButton = document.getElementById("add-button");
 addButton.innerHTML = render;
 
 // list section
 let ListContent = [];
 
 // functions
-// status checkbox
-
-const statusCheckBox = (e) => {
-  const list = e.target.parentNode.parentNode;
-  list.children[0].children[1].classList.toggle('strike');
-  const completed = !list.classList.contains('disabled');
-  list.classList.toggle('disabled');
-  const getLists = document.querySelector('#ulList').children;
-  const index = Array.from(getLists).indexOf(list);
-  ListContent[index].completed = completed;
-  localStorage.setItem('tasks', JSON.stringify(ListContent));
-};
 
 // sorting index values
 const sortIndex = () => {
@@ -32,14 +21,14 @@ const sortIndex = () => {
     description: item.description,
     completed: item.completed,
   }));
-  localStorage.setItem('tasks', JSON.stringify(ListContent));
+  localStorage.setItem("tasks", JSON.stringify(ListContent));
 };
 
 // update local storage
 const updateLocalStorage = (arr, item) => {
   const index = Array.from(arr).indexOf(item.parentNode.parentNode);
   ListContent[index].description = item.value;
-  localStorage.setItem('tasks', JSON.stringify(ListContent));
+  localStorage.setItem("tasks", JSON.stringify(ListContent));
 };
 
 // input validation
@@ -53,12 +42,12 @@ const isValid = (value) => {
 // after editing todo list - hit enter to save
 const saveValue = (event) => {
   if (
-    event.target.classList.contains('todo-description')
-    && event.keyCode === 13
+    event.target.classList.contains("todo-description") &&
+    event.keyCode === 13
   ) {
     event.target.disabled = true;
     event.target.parentNode.parentNode.children[1].src = Image1;
-    const todoContent = document.querySelector('#ulList').children;
+    const todoContent = document.querySelector("#ulList").children;
     updateLocalStorage(todoContent, event.target);
     sortIndex();
   }
@@ -68,23 +57,23 @@ const saveValue = (event) => {
 const todoEdit = (event) => {
   if (event.target.src === Image1) {
     event.target.src = Image2;
-    event.target.parentNode.children[0].children[1].removeAttribute('disabled');
+    event.target.parentNode.children[0].children[1].removeAttribute("disabled");
   } else {
-    const lists = document.querySelector('#ulList').children;
+    const lists = document.querySelector("#ulList").children;
     const indexList = Array.from(lists).indexOf(event.target.parentNode);
     ListContent.splice(indexList, 1);
-    localStorage.setItem('tasks', JSON.stringify(ListContent));
+    localStorage.setItem("tasks", JSON.stringify(ListContent));
     event.target.parentNode.remove();
     sortIndex();
   }
 };
 
 // to check key press enter
-document.addEventListener('keypress', saveValue);
+document.addEventListener("keypress", saveValue);
 
 // add function
 const addtodo = (event) => {
-  const todoInput = document.querySelector('.todo-input').value;
+  const todoInput = document.querySelector(".todo-input").value;
   const valid = isValid(todoInput); // checking input validation
 
   if (!valid) return;
@@ -95,31 +84,33 @@ const addtodo = (event) => {
     description: todoInput,
     completed: false,
   });
-  const ListElement = document.createElement('li');
+  const ListElement = document.createElement("li");
   ListElement.id = ListContent[index].index;
-  const menuIcon = document.createElement('img');
+  const menuIcon = document.createElement("img");
   menuIcon.src = Image1;
-  menuIcon.alt = 'menu';
-  menuIcon.className = 'todo-menu';
+  menuIcon.alt = "menu";
+  menuIcon.className = "todo-menu";
   const ListTemplate = `<div class="list-content">
     <input type="checkbox" class="checkbox">
     <input type="text" value="${ListContent[index].description}" class="todo-description" disabled>
     </div>`;
 
-  menuIcon.addEventListener('click', todoEdit);
+  menuIcon.addEventListener("click", todoEdit);
   ListElement.innerHTML = ListTemplate;
   ListElement.appendChild(menuIcon);
-  const ListSection = document.getElementById('ulList');
+  const ListSection = document.getElementById("ulList");
   ListSection.appendChild(ListElement);
-  const getCheckBoxes = document.querySelectorAll('.checkbox');
+  const getCheckBoxes = document.querySelectorAll(".checkbox");
   const checkBoxIndex = getCheckBoxes.length - 1;
-  getCheckBoxes[checkBoxIndex].addEventListener('change', statusCheckBox);
-  localStorage.setItem('tasks', JSON.stringify(ListContent));
-  document.querySelector('.todo-input').value = null;
+  getCheckBoxes[checkBoxIndex].addEventListener("change", (event) =>
+    statusCheckBox(event, ListContent)
+  );
+  localStorage.setItem("tasks", JSON.stringify(ListContent));
+  document.querySelector(".todo-input").value = null;
 };
 
 // adds the task from input
-addButton.addEventListener('click', addtodo);
+addButton.addEventListener("click", addtodo);
 
 // create list for reloading
 const addingTodos = (task) => {
@@ -129,25 +120,27 @@ const addingTodos = (task) => {
     description: task.description,
     completed: task.completed,
   });
-  const ListElement = document.createElement('li');
+  const ListElement = document.createElement("li");
   ListElement.id = task.index;
-  const menuIcon = document.createElement('img');
+  const menuIcon = document.createElement("img");
   menuIcon.src = Image1;
-  menuIcon.alt = 'menu';
-  menuIcon.className = 'todo-menu';
+  menuIcon.alt = "menu";
+  menuIcon.className = "todo-menu";
   const ListTemplate = `<div class="list-content">
     <input type="checkbox" class="checkbox" >
     <input type="text" value="${task.description}" class="todo-description" disabled>
     </div>`;
 
-  menuIcon.addEventListener('click', todoEdit);
+  menuIcon.addEventListener("click", todoEdit);
   ListElement.innerHTML = ListTemplate;
   ListElement.appendChild(menuIcon);
-  const ListSection = document.getElementById('ulList');
+  const ListSection = document.getElementById("ulList");
   ListSection.appendChild(ListElement);
-  const getCheckBoxes = document.querySelectorAll('.checkbox');
+  const getCheckBoxes = document.querySelectorAll(".checkbox");
   const checkBoxIndex = getCheckBoxes.length - 1;
-  getCheckBoxes[checkBoxIndex].addEventListener('change', statusCheckBox);
+  getCheckBoxes[checkBoxIndex].addEventListener("change", (event) =>
+    statusCheckBox(event, ListContent)
+  );
   if (task.completed === true) {
     getCheckBoxes[checkBoxIndex].click();
   }
@@ -155,19 +148,19 @@ const addingTodos = (task) => {
 
 // show todolist
 const showTodoList = () => {
-  const grabList = JSON.parse(localStorage.getItem('tasks'));
+  const grabList = JSON.parse(localStorage.getItem("tasks"));
   if (grabList === null) return;
   grabList.forEach((item) => addingTodos(item));
-  localStorage.setItem('tasks', JSON.stringify(ListContent));
+  localStorage.setItem("tasks", JSON.stringify(ListContent));
 };
 
 const clearAll = () => {
-  const disabled = document.querySelectorAll('.disabled');
+  const disabled = document.querySelectorAll(".disabled");
   Array.from(disabled).forEach((item) => item.remove());
   ListContent = ListContent.filter((item) => item.completed === false);
   sortIndex();
 };
 
-document.querySelector('#clear').addEventListener('click', clearAll);
+document.querySelector("#clear").addEventListener("click", clearAll);
 
 showTodoList();
